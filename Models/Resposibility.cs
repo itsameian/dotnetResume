235aea1811078685918b25b5e88304f2501e0d76
Ian <iking31415@gmail.com>
2020-07-28 19:14:05 -0600

    Reboot

--- Models/Resposibility.cs
+++
@@ -1,8 +0,0 @@
-namespace dotnetResume.Models{
-    class Responsibility {
-        public int Id {get; set;}
-        public string Description { get; set; }
-        public int JobId { get; set; }
-        public Job Job { get; set; }
-    }
-}
--- Models/Skill.cs
+++
@@ -1,8 +0,0 @@
-namespace dotnetResume.Models{
-    class Skill{
-        public int Id{get; set;}
-        public string SkillName{get; set;}
-        public string Description{get; set; }
-        public int Years { get; set;}
-    }
-}
