6648210f34b50580d93fd7482c377de657cd5d1f
Ian <iking31415@gmail.com>
2020-07-29 18:43:42 -0600

    bugfix, corrected renamed references to resume service

--- Controllers/ResumeController.cs
+++ Controllers/ResumeController.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using dotnetResume.Models;
-using dotnetResume.Services.JobService;
+using dotnetResume.Services.ResumeService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnetResume.Controllers
@@ -11,29 +11,29 @@ namespace dotnetResume.Controllers
     [Route("[controller]")]
     public class ResumeController : ControllerBase
     {
-        private readonly IJobService _jobService;
-        public ResumeController(IJobService jobService)
+        private readonly IResumeService _resumeService;
+        public ResumeController(IResumeService resumeService)
         {
-            this._jobService = jobService;
+            this._resumeService = resumeService;
 
         }
 
         [HttpGet("AllJobs")]
         public IActionResult GetJobs()
         {
-            return Ok(_jobService.GetAllJobs());
+            return Ok(_resumeService.GetAllJobs());
         }
 
         [HttpGet("{id}")]
         public IActionResult GetJob(int id)
         {
-            return Ok(_jobService.GetJobById(id));
+            return Ok(_resumeService.GetJobById(id));
         }
 
         [HttpPost]
         public IActionResult PostJob(Job newJob)
         {
-            return Ok(_jobService.AddJob(newJob));
+            return Ok(_resumeService.AddJob(newJob));
         }
     }
 }
