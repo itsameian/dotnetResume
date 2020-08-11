bb3d5ad33a755ac034394024b5994b4c718bdacd
Ian <iking31415@gmail.com>
2020-08-11 08:37:04 -0600

    updates to file structure

---
+++ client/src/components/Resume/Jobs/Job/Job.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Job = props => (
+  <div>
+    
+  </div>
+);
+
+export default Job;
--- client/src/containers/Resume/Resume.js
+++ client/src/containers/Resume/Resume.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+import classes from './Resume.modules.css';
 
 class Resume extends React.Component {
     render() {
         return (
-            <div className='resume'>
-
+            <div className={classes.Resume}>
+                <Job></Job>
             </div>
         )
     }
